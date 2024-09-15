8ebfc8302f05bb9d44fe181d4f8b288338728605
Paulramirezz <ramirezjohnpaul747@gmail.com>
2024-09-15 12:37:43 +0800

    Create Favorite Actor.js

---
+++ Favorite Actor.js
@@ -0,0 +1,20 @@
+//Create a variable named favActorFirstName and store your favorite actor's first name
+let favActorFirstName = "Gojo";
+//Create a variable named favActorLastName and store their last name
+let favActorLastName = "Saturo";
+let fullName = favActorFirstName + "" + favActorLastName;
+
+let uppercase = fullName.toLowerCase();
+
+let lowercase = "Dog".toUpperCase();
+
+let message = "My favorite actor is " + fullName.toUpperCase();
+
+message += ",his best show is Jujutsu Kaisen .";
+
+console.log(favActorFirstName);
+console.log(favActorLastName);
+console.log(fullname);
+console.log(uppercase);
+console.log(lowercase);
+console.log(message);
